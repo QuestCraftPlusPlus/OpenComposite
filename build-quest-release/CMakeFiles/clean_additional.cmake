f490843087dff3e974b68dd32622955069d6067e
The Judge <53906078+thejudge156@users.noreply.github.com>
2024-01-04 18:29:58 -0800

    Delete build-quest-release directory

--- build-quest-release/CMakeFiles/clean_additional.cmake
+++
@@ -1,9 +0,0 @@
-# Additional clean files
-cmake_minimum_required(VERSION 3.16)
-
-if("${CONFIG}" STREQUAL "" OR "${CONFIG}" STREQUAL "Release")
-  file(REMOVE_RECURSE
-  "bin"
-  "generated"
-  )
-endif()
